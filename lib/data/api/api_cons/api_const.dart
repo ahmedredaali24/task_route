965f4c8b7561a70d6f1c8c524ea7a33566070997
ahmedreda24 <masry5357@gmail.com>
2024-07-12 20:32:02 +0300

    Linking data to ui by cubit. Complete ui processing

--- lib/data/api/api_cons/api_const.dart
+++ lib/data/api/api_cons/api_const.dart
@@ -1,8 +1,8 @@
 class ApiConst {
   /// https://dummyjson.com/products
   static const String baseUrl = "dummyjson.com";
-
 }
-class EndPoints{
+
+class EndPoints {
   static const String productApi = "/products";
 }
